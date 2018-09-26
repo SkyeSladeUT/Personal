7370fce1fc3cb04fa8d3b07526b71ad44e6a4917
Unknown <skyesladeut@outlook.com>
2018-09-26 17:43:23 -0600

    Game Idea

---
+++ Speedy Fish/Assets/Scripts/BoolInput.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+[CreateAssetMenu]
+public class BoolInput : FloatData
+{
+    public string InputType;
+
+    public override float Value
+    {
+
+        get { return Input.GetButtonDown(InputType) ? value : 0; }
+    }
+
+
+}
---
+++ Speedy Fish/Assets/Scripts/FloatData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+[CreateAssetMenu(fileName = "NewFloat")]
+public class FloatData : ScriptableObject
+{
+	public float value;
+	public virtual float Value
+	{
+		get {return value;}
+	}
+}
