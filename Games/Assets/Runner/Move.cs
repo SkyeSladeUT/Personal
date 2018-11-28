86df150a316dad948c3f576d75dabacf0a303418
Unknown <skyesladeut@outlook.com>
2018-11-27 17:59:29 -0700

    worked on runner game

---
+++ Games/Assets/Runner/Move.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+	public float addZ;
+	private Vector3 position;
+	
+	public void MoveBack()
+	{
+		position = transform.position;
+		position.z += addZ;
+		transform.position = position;
+	}
+}
