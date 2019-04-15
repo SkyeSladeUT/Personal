dffe47b90a7f7ba1d53494ff332bc401f2f4b3dd
Unknown <skyesladeut@outlook.com>
2019-04-15 15:11:30 -0600

    Pipe Puzzle

--- Unity/SummerGameScripts/Assets/Scripts/Draggable.cs
+++ Unity/SummerGameScripts/Assets/Scripts/Draggable.cs
@@ -5,7 +5,25 @@ using UnityEngine.Experimental.UIElements;
 
 public class Draggable : MonoBehaviour
 {
-	
 
-	
+	private Vector3 screenPoint, curScreenPoint, curPosition;
+	private float ObjectZ;
+
+
+	void OnMouseDown()
+	{
+		ObjectZ = transform.localPosition.z;
+		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.localPosition);
+	}
+ 
+	void OnMouseDrag()
+	{
+		curScreenPoint.Set(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+		curPosition.z = ObjectZ;
+		transform.localPosition = curPosition;
+		Debug.Log(curPosition);
+
+	}
+
 }
