e0878a986d1fd79183af5191f0896455c0aae758
Unknown <skyesladeut@outlook.com>
2019-04-22 21:51:54 -0600

    Pipe Game

---
+++ Unity/SummerGameScripts/Assets/Scripts/PipeEntrance.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class PipeEntrance : ScriptableObject
+{
+   public bool isConnected;
+   public int pipeConnectionNum, pipeNum;
+   
+
+   public void unConnect()
+   {
+      isConnected = false;
+      pipeConnectionNum = 0;
+   }
+
+   public void Connect(int pipeNum)
+   {
+      pipeConnectionNum = pipeNum;
+      isConnected = true;
+   }
+}
---
+++ Unity/SummerGameScripts/Assets/Scripts/PipePuzzleScript.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipePuzzleScript : MonoBehaviour
+{
+    public List<PipeObject> Pipes;
+}
