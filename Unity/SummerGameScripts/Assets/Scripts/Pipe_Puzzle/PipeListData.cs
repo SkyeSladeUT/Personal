d21228ed154a7422c33f4b4bcbcb5ee37a34a8ec
Unknown <skyesladeut@outlook.com>
2019-05-14 08:27:33 -0600

    Moved Summer Game to New repository

--- Unity/SummerGameScripts/Assets/Scripts/Pipe_Puzzle/PipeListData.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(menuName = "Pipe/List", fileName = "Pipe_List")]
-public class PipeListData : ScriptableObject
-{
-    public List<Single_Pipe_Object> Pipes;
-}
--- Unity/SummerGameScripts/Assets/Scripts/Pipe_Puzzle/Pipe_End_Objects.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Pipe_End_Objects : MonoBehaviour
-{
-    public PipeEntrance entrance;
-}
