403ed3b3e4ce721008d17732ab11feac76dc8cda
Unknown <skyesladeut@outlook.com>
2020-01-30 18:24:55 -0700

    Testing out writing txt file

---
+++ Unity/Script Testing/Assets/CreateFile/TestWrite.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestWrite : MonoBehaviour
+{
+    public InputField userInput;
+    private string _userInputStr;
+    private int _userID;
+    public StringData currentUser;
+    public StringData content;
+
+    public void SetUser()
+    {
+        _userInputStr = userInput.text.Trim();
+        if (int.TryParse(_userInputStr, out _userID) && _userInputStr.Length == 8)
+        {
+            currentUser.value = _userInputStr;
+        }
+        else
+        {
+            Debug.Log("Cannot Convert to User ID");
+        }
+    }
+
+    public void SetContent()
+    {
+        content.value = userInput.text.Trim();
+    }
+    
+}
---
+++ Unity/Script Testing/Assets/CreateFile/WriteToFile.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class WriteToFile : MonoBehaviour
+{
+    public StringData currentUser;
+    private string fileDirectory;
+
+    public bool CheckFile()
+    {
+        int tempval;
+        if (currentUser.value.Length == 8 && int.TryParse(currentUser.value, out tempval))
+        {
+            fileDirectory = Application.dataPath + "/UserData/" + currentUser.value + ".txt";
+            if (!File.Exists(fileDirectory))
+            {
+                Debug.Log("Must Create A User");
+                return false;
+            }
+
+            return true;
+        }
+        else
+        {
+            Debug.Log("Invalid User ID");
+            return false;
+        }
+    }
+    
+    public void CreateFile()
+    {
+        int tempval;
+        if (currentUser.value.Length == 8 && int.TryParse(currentUser.value, out tempval))
+        {
+            fileDirectory = Application.dataPath + "/UserData/" + currentUser.value + ".txt";
+            if (!File.Exists(fileDirectory))
+            {
+                File.WriteAllText(fileDirectory, "" + currentUser.value + " User Log\n\n");
+            }
+        }
+        else
+        {
+            Debug.Log("Invalid User ID");
+        }
+    }
+
+    public void Write(string content)
+    {
+        if(CheckFile())     
+            File.AppendAllText(fileDirectory, content + "\n\n");
+        else
+            Debug.Log("Cannot Write");
+    }
+
+    public void Write(StringData content)
+    {
+        if(CheckFile())  
+           File.AppendAllText(fileDirectory, content.value + "\n\n");
+        else
+            Debug.Log("Cannot Write");
+    }
+
+    public void Login()
+    {
+        if(CheckFile())   
+            File.AppendAllText(fileDirectory, "Login Time: " + System.DateTime.Now + "\n\n");
+        else
+            Debug.Log("Cannot Write");
+    }
+
+    public void Logout()
+    {
+        if (CheckFile())
+        {
+            File.AppendAllText(fileDirectory, "Logout Time: " + System.DateTime.Now + "\n\n");
+            currentUser.value = "";
+        }
+        else
+            Debug.Log("Cannot Write");
+    }
+}
