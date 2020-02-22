c1ff325625b57f325934114bf1ed1e0deceb26e6
Unknown <skyesladeut@outlook.com>
2020-02-21 23:27:53 -0700

    Grass Shader

---
+++ Unity/Shaders/Assets/ShaderFun/Grass/Center.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Center : MonoBehaviour
+{
+    public Material mat;
+
+    private void Update()
+    {
+        mat.SetVector("_Center", transform.position);
+    }
+}
